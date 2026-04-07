f3d7063809b065782742f07f5b2b785faf8be682
Harwyy <my-apple-good@mail.ru>
2026-04-07 19:22:13 +0300

    add menu pag and res flag

--- files/script/get_menu.bat
+++ files/script/get_menu.bat
@@ -4,7 +4,7 @@ set BASE_URL=http://localhost:8080/api/public/restaurants
 set RESTAURANT_ID=8
 
 echo ===== GET /api/public/restaurants/%RESTAURANT_ID%/menu =====
-curl -s "%BASE_URL%/%RESTAURANT_ID%/menu" ^
+curl -s "%BASE_URL%/%RESTAURANT_ID%/menu?page=0&size=10" ^
   -H "Accept: application/json" ^
   | powershell -Command "$raw = ($input | Out-String); try { $raw | ConvertFrom-Json | ConvertTo-Json -Depth 10 } catch { Write-Host 'Ответ не является JSON:' $raw }"
 
--- files/script/get_restaurants.bat
+++ files/script/get_restaurants.bat
@@ -2,11 +2,11 @@
 chcp 65001 > nul
 set BASE_URL=http://localhost:8080/api/public/restaurants
 set CITY=Москва
+set ALL=false
 
-echo ===== GET /api/public/restaurants?city=%CITY% =====
-curl -s -G "%BASE_URL%" --data-urlencode "city=%CITY%" ^
-  -H "Accept: application/json" ^
-  | powershell -Command "$raw = ($input | Out-String); try { $raw | ConvertFrom-Json | ConvertTo-Json -Depth 10 } catch { Write-Host 'Ответ не является JSON:' $raw }"
+echo ===== GET /api/public/restaurants?city=%CITY%&all=%ALL% =====
+
+curl -s -G "%BASE_URL%" --data-urlencode "city=%CITY%" --data-urlencode "all=%ALL%" -H "Accept: application/json" | powershell -Command "$raw = ($input | Out-String); try { $raw | ConvertFrom-Json | ConvertTo-Json -Depth 10 } catch { Write-Host 'Ответ не является JSON:' $raw }"
 
 echo.
 pause
